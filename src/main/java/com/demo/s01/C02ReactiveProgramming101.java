f58a3458feaf981a38f5bb3357a5856078be5eb8
Jason <ya850805@gmail.com>
2022-06-09 02:18:47 +0800

    feat: Add reactive programming demo.

---
+++ src/main/java/com/demo/s01/C02ReactiveProgramming101.java
@@ -0,0 +1,26 @@
+package com.demo.s01;
+
+import com.demo.domain.Book;
+import com.demo.domain.InMemoryDataSource;
+import reactor.core.publisher.Flux;
+
+import java.util.Comparator;
+
+public class C02ReactiveProgramming101 {
+    //返回一個包含每種類別中最貴的書的列表(響應式編程)
+    public static Flux<Book> getMostExpensiveBooksByCategoryReactive(Flux<Book> books) {
+        return books.collectMultimap(Book::getCategory)
+                .flatMapMany(m -> Flux.fromIterable(m.entrySet()))
+                .flatMap(e -> Flux.fromIterable(e.getValue())
+                        .sort(Comparator.comparing(Book::getPrice).reversed())
+                        .next()
+                );
+    }
+
+    public static void main(String[] args) {
+        var pipeline = getMostExpensiveBooksByCategoryReactive(Flux.just(InMemoryDataSource.BOOKS));
+        pipeline = pipeline.doOnNext(System.out::println);
+        System.out.println("什麼都不會發生，直到pipeline開始");
+        pipeline.subscribe();
+    }
+}
